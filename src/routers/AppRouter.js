c1bb2f51292533dff60641fe89da39ab06cdde22
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-13 15:46:25 -0500

    Avances para el router

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { LoginScreen } from '../components/login/LoginScreen';
+import { Marvel } from '../components/marvel/Marvel';
+import { Navbar } from '../components/ui/Navbar';
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+
+        <Switch>
+          <Route exact path="/login" component={LoginScreen}/>
+          <Route exact path="/" component={Marvel}/>
+          
+        </Switch>
+      </div>
+    </Router>
+  );
+}
