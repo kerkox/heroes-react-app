b46ddc9f9a6c5c272bbb9fd2607d844f2037d331
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-13 18:55:40 -0500

    Mejoras para mostrar los datos de los heroes

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,19 +1,15 @@
-import React from 'react'
-import { getHeroesByPubliser } from '../../selectors/getHeroesByPublisher'
+import React from "react";
+import { getHeroesByPubliser } from "../../selectors/getHeroesByPublisher";
+import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
-
   const heroes = getHeroesByPubliser(publisher);
 
   return (
-    <ul>
-      {
-        heroes.map( hero => (
-          <li key={hero.id}>
-            { hero.superhero }
-          </li>
-        ))
-      }      
-    </ul>
-  )
-}
+    <div className="card-columns">
+      {heroes.map((hero) => (
+        <HeroCard key={hero.id} {...hero} />
+      ))}
+    </div>
+  );
+};
