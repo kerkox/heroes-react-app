642ffa088e8b322f77b68cc3eab48616a1cc5467
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-13 15:54:14 -0500

    Segundo Router para la parte interna

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,19 +1,15 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { LoginScreen } from '../components/login/LoginScreen';
-import { Marvel } from '../components/marvel/Marvel';
-import { Navbar } from '../components/ui/Navbar';
+import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRouter = () => {
   return (
     <Router>
       <div>
-        <Navbar />
-
         <Switch>
           <Route exact path="/login" component={LoginScreen}/>
-          <Route exact path="/" component={Marvel}/>
-          
+          <Route path="/" component={DashboardRoutes}/>
         </Switch>
       </div>
     </Router>
