10a29dfaf3db46936936c2c9db4870c5bdd270ce
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-13 19:11:45 -0500

    Mejoras para navegar entre los detalles del Heroe

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Redirect, useParams } from "react-router-dom";
+import { getHeroById } from "../../selectors/getHeroById";
 
-export const HeroScreen = () => {
+export const HeroScreen = ({history}) => {
+  const { heroId } = useParams();
+  const hero = getHeroById(heroId);
+  if (!hero) {
+    return <Redirect to="/" />;
+  }
+  const { superhero, publisher, alter_ego, first_appearance, characters } =
+    hero;
+  const handleRreturn = () => {
+    if( history.length  <= 2) {
+      history.push('/')
+    } else {
+      history.goBack();
+    }
+  };
   return (
-    <div>
-      <h1>Hero Screen</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`../assets/heroes/${heroId}.jpg`}
+          alt={superhero}
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            {" "}
+            <b> Alter ego: </b> {alter_ego}{" "}
+          </li>
+          <li className="list-group-item">
+            {" "}
+            <b> Publisher: </b> {publisher}{" "}
+          </li>
+          <li className="list-group-item">
+            {" "}
+            <b> First appearance: </b> {first_appearance}{" "}
+          </li>
+        </ul>
+        <h5>Characters </h5>
+        <p>{characters}</p>
+        <button className="btn btn-outline-info" onClick={handleRreturn}>
+          Return
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -12,7 +12,7 @@ export const DashboardRoutes = () => {
       <div className="container mt-2">
         <Switch>
           <Route exact path="/marvel" component={MarvelScreen} />
-          <Route exact path="/heroe/:heroeID" component={HeroScreen} />
+          <Route exact path="/hero/:heroId" component={HeroScreen} />
           <Route exact path="/dc" component={DcScreen} />
           <Route path="*" >
             <Redirect to="/marvel" />
