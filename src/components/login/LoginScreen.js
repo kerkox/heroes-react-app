31baa4b011cfad4731141f504dd0253b6a5ef49d
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-15 19:16:23 -0500

    reducer usado en vasrias parte de la aplicacion para el login

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext'
+import { types } from '../../types/types'
 
 export const LoginScreen = ({ history }) => {
 
+  const { dispatch} = useContext(AuthContext)
+
   const handleLogin = () => {
+    const payload = {
+      name:'Paul',
+      logged:true
+    }
+    dispatch({
+      type: types.login,
+      payload
+    });
     history.push('/')
   }
   return (
