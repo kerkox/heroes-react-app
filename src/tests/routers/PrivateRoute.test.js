253661334661e7605cee459d9ab2d01b8e093294
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-17 10:24:04 -0500

    test de PrivateRoute

---
+++ src/tests/routers/PrivateRoute.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { mount } from "enzyme";
+import { PrivateRoute } from "../../routers/PrivateRoute";
+import { MemoryRouter } from "react-router-dom";
+
+describe("Pruebas en <PrivateRoute />", () => {
+  const props = {
+    location: {
+      pathname: "/marvel",
+    },
+  };
+  test("debe de mostrar el componente si está autenticado y guardar localStorage", () => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <PrivateRoute
+          isAuthenticated={true}
+          component={() => <span>Listo</span>}
+          {...props}
+        />
+      </MemoryRouter>
+    );
+
+    expect(wrapper.find('span').exists()).toBe(true)
+  });
+});
