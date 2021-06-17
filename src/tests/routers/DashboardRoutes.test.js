c22d21fe4063f986565be26428a49a8c52ecd71d
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-17 12:09:48 -0500

    pruebas en DashboardRoutes

---
+++ src/tests/routers/DashboardRoutes.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { mount } from "enzyme";
+import { DashboardRoutes } from "../../routers/DashboardRoutes";
+import { AuthContext } from "../../auth/AuthContext";
+import { MemoryRouter } from "react-router-dom";
+
+describe("Pruebas en <DashboardRotes />", () => {
+  const contextValue = {
+    dispatch: jest.fn(),
+    user: {
+      logged: true,
+      name:'Paul'
+    },
+  };
+  test("debe de mostrarse correctamente", () => {
+    const wrapper = mount(
+      <AuthContext.Provider value={contextValue}>
+        <MemoryRouter>
+          <DashboardRoutes />
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('.text-info').text().trim()).toBe(contextValue.user.name)
+  });
+});
